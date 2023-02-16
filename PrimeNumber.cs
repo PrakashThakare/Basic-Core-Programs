c692788e3707f696e99215432dc5c722b5eaae46
Shree Thakre <shreet365@gmail.com>
2023-02-16 09:36:58 +0530

    Check Enter Number is Prime or Not

---
+++ PrimeNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class PrimeNumber
+    {
+        public void GetPrimeNumber()
+        {
+            int i;
+            Console.WriteLine("Enter Number For Check Prime Number Or Not");
+            int iValue = Convert.ToInt32(Console.ReadLine());
+            for (i = 2; i < iValue; i++) 
+            {
+                if(iValue % i == 0)
+                {
+                    Console.WriteLine("It is Not Prime Number");
+                    return;
+                }
+            }
+            Console.WriteLine("It Is Prime Number");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("************** LOGICAL PROGRAMS ****************");
-            Console.WriteLine("Enter Number :\n1.Flip Coin\n2.Leap Year");
+            Console.WriteLine("Enter Number :\n1.Flip Coin\n2.Leap Year\n3.Prime Number");
             int iValue = Convert.ToInt32(Console.ReadLine());
             switch(iValue)
             {
@@ -18,6 +18,10 @@
                     obj.GetLeapYear();
                     break;
                 case 3:
+                    PrimeNumber primeNumber = new PrimeNumber();
+                    primeNumber.GetPrimeNumber();
+                    break;
+                case 4:
                     default:
                     Console.WriteLine("Enter the Wrong Number Please Enter Again");
                     break;
